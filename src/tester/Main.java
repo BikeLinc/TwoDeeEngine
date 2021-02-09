9a0fff1b082a7824eb4e5b0317b3baad7d66e7ee
Lincoln Scheer <lincoln.cobra@gmail.com>
2021-02-08 18:35:58 -0800

    Working On Asset Tool

--- src/tester/Main.java
+++ src/tester/Main.java
@@ -1,9 +1,13 @@
 package tester;
 
+import editor.AssetTool;
+
 public class Main {
 
 	public static void main(String[] args) {
-		MainGameLoop game = new MainGameLoop();
-		game.start();
+		//MainGameLoop game = new MainGameLoop();
+		//game.start();
+                AssetTool assetTool = new AssetTool();
+                assetTool.run();
 	}
 }
